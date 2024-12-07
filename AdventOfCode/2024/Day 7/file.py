9c970f7373be4ea66fc57aa00f1fac32c0357216
Simon Jacobsson <nomis.katt321@gmail.com>
2024-12-07 14:20:42 +0100

    Day 7 complete

--- AdventOfCode/2024/Day 7/file.py
+++ AdventOfCode/2024/Day 7/file.py
@@ -2,63 +2,43 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent
 TEXT_FILE = ROOT_DIR / 'input.txt'
 
+from itertools import product
+
 totalSum = 0
 
-
-
-
-
-
-
-
 def DetermineOperations(numberOfNumbers):
-    tempList = []
-    temp = []
-    for j in range(numberOfNumbers -1):
-        for i in range(numberOfNumbers):
-            if(i % 2 == 0):
-                temp.append("*")
-            else:
-                temp.append("+")
-        tempList.append(temp)
-        
-
-
-    print(tempList, numberOfNumbers)
-    return tempList
-
-
+    return list(product("+*|", repeat=numberOfNumbers - 1))  
 
 def CanBeCalculated(result, numbersList):
     #numbers of different * and + is the len of the numbers 
     # 2 numbers has 2 different possibilites
     numbersList = [int(item) for item in numbersList]
     listOfOperations = DetermineOperations(len(numbersList))
-
-    for j in range(len(listOfOperations)):
-        i = 1
+    for operations in listOfOperations:
         tempsum = numbersList[0]
-        for operation in listOfOperations[j]:
+        for i, operation in enumerate(operations):
             if(operation == "*"):
-                tempsum *= numbersList[i]
+                tempsum *= numbersList[i+1]
+            elif(operation == "+"):
+                tempsum += numbersList[i+1]
             else:
-                tempsum += numbersList[i]
-            i += 1 
+                tempsum = int(str(tempsum) + str(numbersList[i+1]))
         if(tempsum == result):
-            print(tempsum)
+            #print(tempsum, operations)
             return True  
     return False
 
-
-
-
+h = 0
 with open(TEXT_FILE) as f:
     for line in f:
+        print(float((h/850)*100), "%")
         line = line.replace("\n", "")
         lineSplit = line.split(":")
         numbers = str.strip(lineSplit[1]).split(" ")
 
         if(CanBeCalculated(int(lineSplit[0]), numbers)):
             totalSum += int(lineSplit[0])
+        h+=1
+
 
 print(totalSum)
