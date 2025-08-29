49decccc7521bf133867932d1bae05150550b6db
Simon Jacobsson <nomis.katt321@gmail.com>
2025-08-29 23:41:31 +0200

    trying

--- AdventOfCode/2024/Day 10/file.py
+++ AdventOfCode/2024/Day 10/file.py
@@ -2,36 +2,36 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent
 TEXT_FILE = ROOT_DIR / 'input.txt'
 
-
 inputList =[]
 startPositions = []
 scoreValues = {}
 
-def check_around_position(currentPosition, value, list):
-    templist = list
+def check_around_position(currentPosition, value, start):
     left = [-1 + currentPosition[0],currentPosition[1]]
     right = [1 + currentPosition[0],currentPosition[1]]
     up = [currentPosition[0],-1 + currentPosition[1]]
     down = [currentPosition[0],1 + currentPosition[1]]
 
     if value == 9:
-        #print(currentPosition)
-        scoreValues[currentPosition[0], currentPosition[1]] += 1
+        print(currentPosition)
+        scoreValues[start[0], start[1]] += 1
+        return
+
 
     moveright = can_move_direction(right, value)
     moveleft = can_move_direction(left, value)
     moveup = can_move_direction(up, value)
     movedown = can_move_direction(down, value)
 
+    print(value)
     if moveleft:
-        check_around_position(left, value+1, templist)
+        check_around_position(left, value+1, start)
     if moveright:
-        check_around_position(right, value+1, templist)
+        check_around_position(right, value+1, start)
     if moveup:
-        check_around_position(up, value+1, templist)
+        check_around_position(up, value+1, start)
     if movedown:
-        check_around_position(down, value+1, templist)
-    print(templist)
+        check_around_position(down, value+1, start)
 
 def can_move_direction(currentPosition, value):
     if  valid_position(currentPosition) and inputList[currentPosition[1]][currentPosition[0]] == value+1:
@@ -40,7 +40,7 @@ def can_move_direction(currentPosition, value):
         return False
 
 def valid_position(currentPosition):
-    if (currentPosition[1] < len(inputList) and currentPosition[0] < len(inputList[0])) and (currentPosition[0] > 0 and currentPosition[1] > 0):
+    if (currentPosition[1] < len(inputList) and currentPosition[0] < len(inputList[0])) and (currentPosition[0] >= 0 and currentPosition[1] >= 0):
         return True
     else:
         return False
@@ -55,7 +55,6 @@ with open(TEXT_FILE) as f:
             c = int(c)
             if c == 0:
                 startPositions.append([x,y])
-            elif c == 9:
                 scoreValues[x, y] = 0
             tempList.append(c)
         inputList.append(tempList)
@@ -68,7 +67,7 @@ for x in inputList:
 
 for startposition in startPositions:
     print(startposition)
-    check_around_position(startposition, 0, [])
+    check_around_position(startposition, 0, startposition)
 
 print(scoreValues)
 
