ee19c86da7ec12812bfb95da451e06b754ad6a06
Simon Jacobsson <nomis.katt321@gmail.com>
2025-09-04 20:57:19 +0200

    Fläsk bert

--- AdventOfCode/2024/Day 10/file.py
+++ AdventOfCode/2024/Day 10/file.py
@@ -1,10 +1,16 @@
 from pathlib import Path
+
+from pyparsing import Empty
+
 ROOT_DIR = Path(__file__).parent
 TEXT_FILE = ROOT_DIR / 'input.txt'
 
 inputList =[]
 startPositions = []
 scoreValues = {}
+
+newtemp = []
+tempdict = {}
 
 def check_around_position(currentPosition, value, start):
     left = [-1 + currentPosition[0],currentPosition[1]]
@@ -13,17 +19,23 @@ def check_around_position(currentPosition, value, start):
     down = [currentPosition[0],1 + currentPosition[1]]
 
     if value == 9:
-        print(currentPosition)
-        scoreValues[start[0], start[1]] += 1
+        newtemp[currentPosition[1]][currentPosition[0]] = "X"
+        newtemp[start[1]][start[0]] += 1
+        sasa = (currentPosition[0],currentPosition[1])
+        if sasa not in tempdict: #Ta bort denna om du vill a alla unika väger
+            tempdict[sasa] = 1
+            scoreValues[start[0], start[1]] += 1
         return
 
+
+    #print(".", end="")
 
     moveright = can_move_direction(right, value)
     moveleft = can_move_direction(left, value)
     moveup = can_move_direction(up, value)
     movedown = can_move_direction(down, value)
 
-    print(value)
+    #print(value)
     if moveleft:
         check_around_position(left, value+1, start)
     if moveright:
@@ -49,6 +61,7 @@ def valid_position(currentPosition):
 with open(TEXT_FILE) as f:
     y = 0
     for line in f:
+        empt = []
         tempList = []
         line = line.replace("\n", "")
         for x,c in enumerate(line):
@@ -57,19 +70,27 @@ with open(TEXT_FILE) as f:
                 startPositions.append([x,y])
                 scoreValues[x, y] = 0
             tempList.append(c)
+            empt.append(".")
         inputList.append(tempList)
         y += 1
 
-for x in inputList:
-    print(x)
-
-
 
 for startposition in startPositions:
-    print(startposition)
+    for y in range(0, len(inputList)):
+        temppp = []
+        for x in range(0, len(inputList[0])):
+            temppp.append(".")
+        newtemp.append(temppp)
+    newtemp[startposition[1]][startposition[0]] = 0 #ojjj
+    tempdict = {}
     check_around_position(startposition, 0, startposition)
+
+    newtemp.clear()
 
 print(scoreValues)
 
+total = 0
+for value in scoreValues.values():
+    total += value
 
-
+print(total)
