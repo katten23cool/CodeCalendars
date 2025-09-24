03899a21e1dc57d77f21b9658f8a4e3aac4430af
Simon Jacobsson <nomis.katt321@gmail.com>
2025-09-24 15:56:11 +0200

    Day 12

--- AdventOfCode/2024/Day 12/file.py
+++ AdventOfCode/2024/Day 12/file.py
@@ -4,6 +4,7 @@ TEXT_FILE = ROOT_DIR / 'input.txt'
 
 dict = {}
 areas = []
+map = []
 class Area:
     def __init__(self, letter):
         self.letter = letter
@@ -14,18 +15,45 @@ class Area:
             return True
         return False
 
+    def posindict(self, pos):
+        if pos in self.dict:
+            return True
+        return False
+    def addtodict(self, pos):
+        if pos in self.dict:
+            self.dict[pos] += 1
+        else:
+            self.dict[pos] = 1
+
 
 with open(TEXT_FILE) as f:
     for line in f:
         line = line.replace("\n", "")
+        templ = []
         for c in line:
-            if c in dict:
-                dict[c] += 1
-            else:
-                dict[c] = 1
+            templ.append(c)
+        map.append(templ)
 
+print(map)
+def checkbeside(area, position):
+    posX = [position[0] + 1, position[1]]
+    posY = [position[0], position[1]+ 1]
+    letterX = map[posX[0]][posX[1]]
+    letterY = map[posY[0]][posY[1]]
+    if letterX is area.letter:
+        area.addtodict(posX)
+        checkbeside(area, letterX)
+    if letterY is area.letter:
+        area.addtodict(posY)
+        checkbeside(area, letterY)
 
 nb_areas = 0
-def findarea():
-    pass
+def findarea(letter, startpos):
+    area = Area(letter)
+    checkbeside(area, startpos)
+
+    return area
+
+print(findarea("A", [0,0]).dict)
+
 print(dict)
