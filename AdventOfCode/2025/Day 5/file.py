412375f09361bede6d8e917f6c0e2b4fc1357926
Simon Jacobsson <nomis.katt321@gmail.com>
2025-12-07 14:31:29 +0100

    I caved

--- AdventOfCode/2025/Day 5/file.py
+++ AdventOfCode/2025/Day 5/file.py
@@ -1,54 +1,51 @@
 from pathlib import Path
+
 ROOT_DIR = Path(__file__).parent
 TEXT_FILE = ROOT_DIR / 'input.txt'
 
-freshtotal = 0
+fresh_ranges = []
 checkFresh = True
-freshIngredients = []
+freshtotal = 0
+totalsumingredients = 0
 
 with open(TEXT_FILE) as f:
     for line in f:
-        line = line.replace("\n", "")
-
+        line = line.strip()
         if checkFresh:
             if line == "":
                 checkFresh = False
-                print("damn")
+                continue
 
-            if checkFresh:
-                splits = line.split("-")
-                num1 = int(splits[0])
-                num2 = int(splits[1])
+            a, b = map(int, line.split("-"))
+            fresh_ranges.append((a, b))
 
-                freshIngredients.append([num1, num2])
         else:
             num = int(line)
-            print(line)
-            for ingredient in freshIngredients:
-                if ingredient[0] < num < ingredient[1]:
+            for a, b in fresh_ranges:
+                if a <= num <= b:
                     freshtotal += 1
                     break
 
-totalsumingredients = 0
-somethingchanged = True
-changed = 0
-while somethingchanged:
-    changed = 0
-    for i in range(len(freshIngredients)):
-        for o in range(len(freshIngredients)):
-            if freshIngredients[o][0] < freshIngredients[i][0] < freshIngredients[o][1]:
-                freshIngredients[i][0] = freshIngredients[o][1]+1
-                changed += 1
-    if changed > 0:
-        somethingchanged = True
+# --- MERGE RANGES CORRECTLY ---
+fresh_ranges.sort()  # sort by start
+merged = []
+
+for start, end in fresh_ranges:
+    if not merged:
+        merged.append([start, end])
     else:
-        somethingchanged = False
+        last = merged[-1]
+        if start <= last[1] + 1:  # Overlapping or touching
+            last[1] = max(last[1], end)
+        else:
+            merged.append([start, end])
 
-for ingredient in freshIngredients:
-    if ingredient[1]-ingredient[0] != 0:
-        totalsumingredients += ingredient[1]-ingredient[0]+1
-        print(ingredient, ingredient[1]-ingredient[0]+1)
+# compute total unique range length
+for a, b in merged:
+    totalsumingredients += b - a + 1
 
-print(freshIngredients)
-print(freshtotal)
-print(totalsumingredients)
+print("Merged ranges:", merged)
+print("Fresh total:", freshtotal)
+print("Total summed range:", totalsumingredients)
+
+#397665529473088
